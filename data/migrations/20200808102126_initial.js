6f38898920078c9340f308a2ca3c15613652d1eb
Anthony Sutton <anthony.sutton95@gmail.com>
2020-08-08 10:48:50 -0700

    feat(tables): Created database tables
    Signed-off-by: Anthony Sutton <anthony.sutton95@gmail.com>

--- data/migrations/20200808102126_initial.js
+++ data/migrations/20200808102126_initial.js
@@ -1,8 +1,59 @@
+exports.up = async function (knex) {
+	await knex.schema.createTable("recipes", (table) => {
+		table.increments("id");
+		table.text("name").notNullable().unique();
+	});
 
-exports.up = function(knex) {
-  
+	await knex.schema.createTable("ingredients", (table) => {
+		table.increments("id");
+		table.text("name").notNullable().unique();
+	});
+
+	await knex.schema.createTable("instructions", (table) => {
+		table.increments("id");
+		table.text("instruction").notNullable();
+	});
+
+	await knex.schema.createTable("recipes_ingredients", (table) => {
+		table.primary(["recipe_id", "ingredient_id"]);
+		table
+			.integer("recipe_id")
+			.references("id")
+			.inTable("recipes")
+			.onDelete("CASCADE")
+			.onUpdate("CASCADE");
+		table
+			.integer("ingredient_id")
+			.references("id")
+			.inTable("ingredients")
+			.onDelete("CASCADE")
+			.onUpdate("CASCADE");
+		table.float("quantity").notNullable();
+	});
+
+	await knex.schema.createTable("recipes_instructions", (table) => {
+		table.primary(["recipe_id", "instruction_id"]);
+		table
+			.integer("recipe_id")
+			.references("id")
+			.inTable("recipes")
+			.onDelete("CASCADE")
+			.onUpdate("CASCADE");
+		table
+			.integer("instruction_id")
+			.references("id")
+			.inTable("instructions")
+			.onDelete("CASCADE")
+			.onUpdate("CASCADE");
+		table.integer("step_number").notNullable().unsigned();
+	});
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function (knex) {
+	await knex.schema.dropTableIfExists("recipes_instructions");
+	await knex.schema.dropTableIfExists("recipes_ingredients");
+	await knex.schema.dropTableIfExists("recipes_instructions");
+	await knex.schema.dropTableIfExists("instructions");
+	await knex.schema.dropTableIfExists("ingredients");
+	await knex.schema.dropTableIfExists("recipes");
 };
